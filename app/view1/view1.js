30f41515c60d79be6a663fdfaccda64e6b22960c
Cameron Moss <ssomnoremac@gmail.com>
2015-02-17 15:41:00 +0000

    add support for legacy browers without Promises

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -19,7 +19,7 @@ angular.module('myApp.view1', ['ngRoute','ngResource'])
 .controller('View1Ctrl', ['$scope','Books',
   function($scope, Books){
     $scope.expo = {};
-    // function to handle click evenr and toggle "expanded" class of library items
+    // function to handle click event and toggle "expanded" class of library items
     $scope.expand = function (id){
       if($scope.expo[id]) {
         $scope.expo[id] = false;
@@ -27,7 +27,8 @@ angular.module('myApp.view1', ['ngRoute','ngResource'])
         $scope.expo[id] = true;
       }
     };
-    // create a prommise to return a filtered array from Book resource
+    // create a promise to return a filtered array from Book resource
+    if (Promise){
     var promise = new Promise(function(resolve,reject,data){
       Books.query(function(data){
         var bookArray = [];
@@ -48,7 +49,7 @@ angular.module('myApp.view1', ['ngRoute','ngResource'])
     promise.then(function(result){
       // create a list of languages and tags
         var languageList = [];
-        var tagList = []
+        var tagList = [];
         result.forEach(function(book){
           if (book.doc){
             book.doc.languages.forEach(function(lang){
@@ -80,5 +81,48 @@ angular.module('myApp.view1', ['ngRoute','ngResource'])
           $scope.languageSelected = "selected";
           };
         });
+    } //promises supported above, if not, at least get everything eventually
+    else{
+      var bookArray = [];
+      Books.query(function(data){
+        var keys = Object.keys(data);
+        for (var i = 0; i < keys.length; i++) {
+          bookArray[i] = data[keys[i]];
+        } 
+        $scope.booklist = bookArray;
+      });
+      var languageList = [];
+      var tagList = [];
+      bookArray.forEach(function(book){
+          if (book.doc){
+            book.doc.languages.forEach(function(lang){
+              languageList.push(lang);
+            });
+            book.doc.tags.forEach(function(tag){
+              tagList.push(tag);
+            });
+          }
+      });
+      // remove duplicates
+      var uniqueLanguageList = languageList.filter(function(elem, pos) {
+            return languageList.indexOf(elem) == pos;
+          });
+      var uniqueTagList = tagList.filter(function(elem, pos) {
+            return tagList.indexOf(elem) == pos;
+         });
+      $scope.languages = uniqueLanguageList;
+      $scope.tags = uniqueTagList;
+      $scope.predicate = 'doc.name';
+      $scope.languageButtonTitle = 'Language';
+      $scope.collapsed = false;
+      $scope.filterByTag = function(tag){
+        $scope.filterTag = tag;
+      };
+      $scope.updateLanguage = function(selectedLanguage) {
+        $scope.languageButtonTitle = selectedLanguage;
+        $scope.filterLanguage = selectedLanguage;
+        $scope.languageSelected = "selected";
+      };
+    }
   }
 ]);
